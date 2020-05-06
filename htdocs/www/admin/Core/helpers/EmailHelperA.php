0ab7bbe93c29cf2fac86b4badf1cdb741b27c2a1
Pedro Mello <pedro_mello@icloud.com>
2020-05-06 17:19:27 -0300

    EmailHelper

--- htdocs/www/admin/Core/helpers/EmailHelperA.php
+++ htdocs/www/admin/Core/helpers/EmailHelperA.php
@@ -38,30 +38,117 @@
 
 class EmailHelperA
 {
-    private static $hostSMTP = "";
-    private static $userSMTP = "";
-    private static $passwordSMTP = "";
-    private static $name = "";
-    private static $from = "";
-    private static $to = "";
-    private static $subject = "";
-    private static $attachments = "";
-    private static $messages = "";
 
-    private static $phpMailer = null;
+    private $mailer = null;
 
-    public static function getInstancePhpMailer() : ?object
+    public static function make() : ?object
     {
-        if(!is_null(self::$phpMailer)){
-            self::$phpMailer = new PHPMailer()->SetLanguage("br", HELPERS . "/Email/language/");
-            return self::$phpMailer;
-        }else{
-            return self::$phpMailer;
+        $instance = new Self();
+        return $instance;
+    }
+
+    public function getInstanceMailer()
+    {
+        if(is_null($this->mailer)){
+            $this->mailer = new PHPMailer();
+            return $this->mailer;
         }
+        return $this->mailer;
+    }
+    public function __construct()
+    {
+
+        $this->mailer = $this->getInstanceMailer();
+        $this->mailer->SetLanguage("br", HELPERS . "/Email/language/");
+
+        $this->mailer->SMTPDebug = 3;
+        $this->mailer->isSMTP();
+        $this->mailer->SMTPAuth = true;
+        $this->mailer->isHTML(true);
+
+        $this->setHost("mail.70e7.com");
+        $this->setUsername("no-reply@70e7.com");
+        $this->setPassword("A123456789b");
+        $this->setPort(587);
+
+        $this->setFrom("no-reply@70e7.com");
+        $this->setFromName("70e7");
+
+
     }
 
-    public static function send()
+
+    public function send()
     {
-        $phpMailer = new PHPMailer();
+        $this->mailer->send();
+        return $this;
     }
+
+    public function setHost($host)
+    {
+        $this->mailer->Host = $host;
+        return $this;
+    }
+
+    public function setUsername($username)
+    {
+        $this->mailer->Username = $username;
+        return $this;
+    }
+
+    public function setPassword($password)
+    {
+        $this->mailer->Password = $password;
+        return $this;
+    }
+
+    public function setPort($port)
+    {
+        $this->mailer->Port = $port;
+        return $this;
+    }
+
+    public function setFrom($from)
+    {
+        $this->mailer->From = $from;
+        return $this;
+    }
+
+    public function setFromName($fromName)
+    {
+        $this->mailer->FromName = $fromName;
+        return $this;
+    }
+
+    public function addAddress($emailAddress, $recipientName)
+    {
+        $this->mailer->AddAddress($emailAddress, $recipientName);
+        return $this;
+    }
+
+    public function setSubject($subject)
+    {
+        $this->mailer->Subject = $subject;
+        return $this;
+    }
+
+    public function AddAttachment($attachment)
+    {
+        $this->mailer->AddAttachment($attachment);
+        return $this;
+    }
+
+    public function setMessage($message)
+    {
+        $this->mailer->Body = $message;
+        return $this;
+    }
+
+    public function setAltMessage($altMessage)
+    {
+        $this->mailer->AltBody = $altMessage;
+        return $this;
+    }
+
+
 }
